554f223a04d46eeea519e4c15f1d08fd870e6b5b
MUKILTEO\1717665 <1717665@mukilteo.wednet.edu>
2023-12-06 13:33:23 -0800

    worked on enemy

---
+++ Assets/Gabby/Scripts/EnemyAI.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public GameObject target;
+
+    public float distance;
+    public int speed = 5; //subject to change
+
+    public float health = 3; //also subject to change
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(target.transform.position, transform.position); //constantly get the distance
+
+        //transform.LookAt(target.transform);
+
+        if (distance > 10) //move closer
+        {
+            if (transform.position.x < target.transform.position.x) //if enemy is left of target, move right
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            if (transform.position.x > target.transform.position.x) //if enemy is right of target, move left
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            if (transform.position.y > target.transform.position.y) //if enemy is above target, move down
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+            if (transform.position.y < target.transform.position.y) //if enemy is below target, move up
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+        }
+        if (distance <= 10) //shoot
+        {
+            
+        }
+        
+    }
+}

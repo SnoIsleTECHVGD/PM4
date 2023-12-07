ffb6f98e5518525256cd9308cda7eaa5ab27b700
MUKILTEO\1717665 <1717665@mukilteo.wednet.edu>
2023-12-07 13:33:27 -0800

    Shooting mechanic (doesn't work yet)

--- Assets/Gabby/Scripts/EnemyAI.cs
+++ Assets/Gabby/Scripts/EnemyAI.cs
@@ -6,11 +6,14 @@ public class EnemyAI : MonoBehaviour
 {
 
     public GameObject target;
+    public GameObject bullet;
 
     public float distance;
     public int speed = 5; //subject to change
 
     public float health = 3; //also subject to change
+
+    public bool hasBullet = true;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +26,6 @@ public class EnemyAI : MonoBehaviour
     {
         distance = Vector3.Distance(target.transform.position, transform.position); //constantly get the distance
 
-        //transform.LookAt(target.transform);
 
         if (distance > 10) //move closer
         {
@@ -44,9 +46,31 @@ public class EnemyAI : MonoBehaviour
                 transform.Translate(Vector3.up * speed * Time.deltaTime);
             }
         }
-        if (distance <= 10) //shoot
+
+        if (distance < 7) //move away
         {
-            
+            if (transform.position.x < target.transform.position.x) //if enemy is left of target, move left
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            if (transform.position.x > target.transform.position.x) //if enemy is right of target, move right
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            if (transform.position.y > target.transform.position.y) //if enemy is above target, move up
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+            if (transform.position.y < target.transform.position.y) //if enemy is below target, move down
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
+
+        if (distance <= 10 && hasBullet) //shoot
+        {
+            bullet.GetComponent<ShootScript>().shootWithCoroutine(target.transform.position.x, target.transform.position.y, transform.position.x, transform.position.y);
+            hasBullet = false;
         }
         
     }
--- Assets/Gabby/Scripts/ShootScript.cs
+++ Assets/Gabby/Scripts/ShootScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShootScript : MonoBehaviour
 {
+    public int speed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,46 @@ public class ShootScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public IEnumerator shoot(float xLocation, float yLocation)
+    {
+        yield return new WaitForSeconds(0.001f);
+        int rise = (int) (yLocation - transform.position.y);
+        int run = (int) (xLocation - transform.position.x);
+        for (int r = 1000; r > 0; r--)
+        {
+            for (int i = rise; i > 0; i--)
+            {
+                if (rise < 0)
+                {
+                    transform.Translate(Vector3.down * speed * Time.deltaTime);
+                }
+                else if (rise > 0)
+                {
+                    transform.Translate(Vector3.up * speed * Time.deltaTime);
+                }
+            }
+            for (int k = run; k > 0; k--)
+            {
+                if (run < 0)
+                {
+                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                else if (run > 0)
+                {
+                    transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }
+            }
+            yield return new WaitForSeconds(0.002f);
+        }
         
+    }
+
+    public void shootWithCoroutine(float xLocation, float yLocation, float xStart, float yStart)
+    {
+      //GetComponent<Transform>().position = new Vector3(xStart, yStart, 0);
+        StartCoroutine(shoot(xLocation, yLocation));
     }
 }

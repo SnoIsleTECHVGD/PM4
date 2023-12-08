1eaa04b5ca3d734c06f909307280f164880472f9
MUKILTEO\1717960 <1717960@mukilteo.wednet.edu>
2023-12-08 12:23:08 -0800

    MainMenu

---
+++ Assets/Lucy/Main Menu Stuff/Buttons/UIButtons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+    public void Start()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

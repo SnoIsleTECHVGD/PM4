1ea8af31bb8cf403b97250333117ecbb34ae0bad
MUKILTEO\1717665 <1717665@mukilteo.wednet.edu>
2023-12-15 12:35:46 -0800

    Worked on barrel

--- Assets/Gabby/Scripts/BarrelScript.cs
+++ Assets/Gabby/Scripts/BarrelScript.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class BarrelScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTrigerEnter2D(Collider2D collision)
     {
-
+        Debug.Log("Barrel trigger test");
+        GetComponent<Animator>().SetInteger("aniRun", 2);
     }
 }

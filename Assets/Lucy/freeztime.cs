2be3dff200801e09b4d65cb597798264742be1a0
MUKILTEO\1717960 <1717960@mukilteo.wednet.edu>
2024-01-04 12:50:18 -0800

    pdused!!!!

--- Assets/Lucy/freeztime.cs
+++ Assets/Lucy/freeztime.cs
@@ -2,14 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class freeztime : MonoBehaviour
 {
     // Start is called before the first frame update
-   
-
+    public KeyCode Esc;
+    public GameObject pause;
     // Update is called once per frame
-  public  void freez(float scale)
+
+
+    public void Start()
     {
-        Time.timeScale = scale;
+        pause.SetActive(false);
     }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(Esc))
+        {
+
+            if (Time.timeScale == 1)
+                freez();
+            else
+                unfreez();
+        }
+    }
+
+    public void freez()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0;
+
+    }
+
+    public void unfreez()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1;
+    }
+
 }
